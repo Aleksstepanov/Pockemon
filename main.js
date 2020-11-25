1e5f355c27aa67106f54e99387a4a4e3370fc650
Aleksey <stepanovl87@gmail.com>
2020-11-25 07:20:49 +0300

    homework 3/4

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ class Pockemon {
         this.damageHP = person.damageHP,
         this.elHP = person.elHP,
         this.elProgressBar = person.elProgressBar,
-        this.$btnDamage = person.$btnDamage;
+        this.$btnDamage = person.$btnDamage,
+        this.click = person.click;
     }
     renderHPLife() {
         this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
@@ -64,7 +65,8 @@ const
         damageHP: 100,
         elHP: document.getElementById('health-character'),
         elProgressBar: document.getElementById('progressbar-character'),
-        $btnDamage: document.getElementById('btn-kick-player')
+        $btnDamage: document.getElementById('btn-kick-player'),
+        click: 6,
     }),
     enemy = new Pockemon ({
         name: 'Charmander',
@@ -72,24 +74,53 @@ const
         damageHP: 100,
         elHP: document.getElementById('health-enemy'),
         elProgressBar: document.getElementById('progressbar-enemy'),
-        $btnDamage: document.getElementById('btn-kick-enemy')
+        $btnDamage: document.getElementById('btn-kick-enemy'),
+        click: 6,
     });
 
 
-const init = () => {
-    console.log('Start Game!');
-    character.renderHP();
-    enemy.renderHP();
-}
-
-
-init();
-character.$btnDamage.addEventListener('click', () => {
-    const {name} = character.name;
-    enemy.damageClick(name);
-});
-enemy.$btnDamage.addEventListener('click', () => {
-    const {name} = enemy.name
-    character.damageClick(name);
-})
-
+    const init = () => {
+        console.log('Start Game!');
+        character.renderHP();
+        enemy.renderHP();
+        character.$btnDamage.innerText = character.$btnDamage.innerText + ' /' + character.click; 
+        enemy.$btnDamage.innerText = enemy.$btnDamage.innerText + ' /' + enemy.click;
+    }
+    
+    function makeCounter() {
+        let count = 0;
+        return function() {
+          return count++;
+        };
+      }
+      
+    const counterCharacter = makeCounter(),
+          counterEnemy = makeCounter();
+    
+    init();
+    character.$btnDamage.addEventListener('click', () => {
+        const {name} = character.name,
+              {click} = character;
+        if (click === counterCharacter) {
+            Object.values(document.querySelectorAll('.button')).forEach(elem => elem.disabled = true);
+            alert('End Game!')
+        }
+        else {
+            character.$btnDamage.innerText = character.$btnDamage.innerText.split('/')[0] + '/' + (click - counterCharacter());
+            enemy.damageClick(name);
+        }
+    });
+    enemy.$btnDamage.addEventListener('click', () => {
+        const {name} = enemy.name,
+              {click} = enemy;
+        if (click === counterCharacter) {
+            Object.values(document.querySelectorAll('.button')).forEach(elem => elem.disabled = true);
+            alert('End Game!')
+        }
+        else {
+            enemy.$btnDamage.innerText = enemy.$btnDamage.innerText.split('/')[0] + '/'  + (click - counterEnemy());
+            character.damageClick(name);
+        }
+         
+    })
+    
