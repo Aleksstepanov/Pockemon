3d5791cdc46e47d3a28138f697420ff1d51da28c
Aleksey <stepanovl87@gmail.com>
2020-11-26 19:52:21 +0300

    homework/5 переделал на модули

--- js/main.js
+++ js/main.js
@@ -28,29 +28,24 @@ const counterCharacter = makeCounter(),
       counterEnemy = makeCounter();
 
 document.querySelector('.control').addEventListener('click', (event) => {
-    if (event.target === character.$btnDamage) {
-        const {name, click} = character;
-    
-    if (click === counterCharacter()) {
-        Object.values(document.querySelectorAll('.button')).forEach(elem => elem.disabled = true);
-        alert('End Game!')
-    }
-    else {
-        character.$btnDamage.innerText = character.$btnDamage.innerText.split('/')[0] + '/' + (click - counterCharacter());
-        enemy.changeHP(random(60, 20), name);
-    }
-    }
-    else 
-    if (event.target === enemy.$btnDamage) {
-        const {name, click} = enemy;
-    
-    if (click === counterEnemy()) {
+    const onClick = (person1, person2, cb) => {
+        const {name, click} = person1;
+        if (click === cb()) {
             Object.values(document.querySelectorAll('.button')).forEach(elem => elem.disabled = true);
             alert('End Game!')
         }
         else {
-            enemy.$btnDamage.innerText = enemy.$btnDamage.innerText.split('/')[0] + '/' + (click - counterEnemy());
-            character.changeHP(random(60, 20), name);
+            person1.$btnDamage.innerText = person1.$btnDamage.innerText.split('/')[0] + '/' + (click - cb());
+            person2.changeHP(random(60, 20), name);
         }
+    }
+    if (event.target === character.$btnDamage) {
+        onClick(character, enemy, counterCharacter);
+        
+    }
+    else 
+    if (event.target === enemy.$btnDamage) {
+        onClick(enemy, character, counterEnemy);
+        
     }
 })
